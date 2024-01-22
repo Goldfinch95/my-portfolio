88610e318e205125da723a5f481be0cf6102e0b6
Goldfinch <facu_vila@hotmail.com>
2024-01-22 20:50:20 -0300

    trabajando en el lado visual del form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavbarComponent, PortfolioComponent],
+  imports: [CommonModule, RouterOutlet, NavbarComponent, PortfolioComponent, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
