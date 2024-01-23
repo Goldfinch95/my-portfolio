b2806622456207038b46794c6256de29de4a1b80
Goldfinch <facu_vila@hotmail.com>
2024-01-22 21:18:50 -0300

    se agrego un footer y se pasaron los textos a español

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,14 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { PortfolioComponent } from './components/portfolio/portfolio.component';
 import { ContactComponent } from './components/contact/contact.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavbarComponent, PortfolioComponent, ContactComponent],
+  imports: [CommonModule, RouterOutlet, NavbarComponent, PortfolioComponent, ContactComponent , FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
