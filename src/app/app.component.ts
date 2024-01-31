e218262f7193e8a22e6df2a9244c54762890dbe4
Goldfinch <facu_vila@hotmail.com>
2024-01-30 22:29:10 -0300

    se agregaron los programas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,19 +5,30 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { PortfolioComponent } from './components/portfolio/portfolio.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { FooterComponent } from './components/footer/footer.component';
-
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {faJs} from '@fortawesome/free-brands-svg-icons'
+import {faReact} from '@fortawesome/free-brands-svg-icons'
+import {faAngular} from '@fortawesome/free-brands-svg-icons'
+import {faFigma} from '@fortawesome/free-brands-svg-icons'
+import { faBootstrap } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavbarComponent, PortfolioComponent, ContactComponent , FooterComponent],
+  imports: [CommonModule, RouterOutlet, NavbarComponent, PortfolioComponent, ContactComponent , FooterComponent, FontAwesomeModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'my-portfolio';
 
+  faJs = faJs
+  faReact = faReact
+  faAngular = faAngular
+  faFigma = faFigma
+  faBootstrap = faBootstrap
+
+  
   toProjects(){
     document.getElementById('projects')?.scrollIntoView({behavior: 'smooth'})
   }
